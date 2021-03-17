fed5fdff9bfb6850029598374bc98e1d05f70f79
emdifiore22 <emdifiore22@gmail.com>
2021-03-17 15:40:48 +0100

    UPDATE 17/03 - COLAB NOTEBOOK UPLOAD
    UPLOAD OF ALL NOTEBOOKS

--- scripts/training_featureextraction.py
+++ scripts/training_featureextraction.py
@@ -13,7 +13,7 @@ N_MELS = 128
 HOP_LENGTH = 512
 N_FFT = 1024
 POWER = 2.0
-FRAME_NUMS = 344
+FRAME_NUMS = 313
 
 # load dataset
 def select_dirs(path):
@@ -42,8 +42,8 @@ def file_to_vector_array(file_name, n_mels=64, n_fft=1024, hop_length=512, power
     y, sr = file_load(file_name)
     mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels, power=power)
     # 02 convert melspectrogram to log mel energy
-    #log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
-    log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
+    log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
+    #log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
     return log_mel_spectrogram
 
   
@@ -64,18 +64,20 @@ def key_by_id(item):
   return machine_id
 
 # load base_directory list
-dirs = "./DATA/ToyCar"
+dirs = "/Volumes/Extreme SSD/Tesi/DATA/valve"
 files = file_list_generator(dirs)
 
 train_data = list_to_vector_array(files, msg="generate train_dataset", n_mels=N_MELS, n_fft=N_FFT, hop_length=HOP_LENGTH, power=POWER)
-print(train_data.shape)
+
 train_data = train_data.reshape(len(files), N_MELS, FRAME_NUMS)
 
-with open('./extractedData/train/training_ToyCar.npy', 'wb') as f:
+print(train_data)
+
+with open('/Volumes/Extreme SSD/Tesi/DATA/training_valve.npy', 'wb') as f:
     numpy.save(f, train_data)
 
 # load base_directory list
 grouped_list_by_machine_id = [list(v) for k,v in groupby(sorted(files), key_by_id)]
 
-with open('./extractedData/train/training_ToyCar_grouped_list.npy', 'wb') as file_pi:
+with open('/Volumes/Extreme SSD/Tesi/DATA/training_valve_grouped_list.npy', 'wb') as file_pi:
     pickle.dump(grouped_list_by_machine_id, file_pi)
